ec50c6fd236c0d442f3af7cb03ea221f42747a6a
liumingzhou <18231133236@163.com>
2021-09-22 23:53:46 +0800

    react-router-dom

--- react-staging/src/App.jsx
+++ react-staging/src/App.jsx
@@ -1,14 +1,50 @@
-import React, { Component } from 'react'
-import Search from './components/Search'
-import List from './components/List'
-
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Header from './components/Header';
+import MyNavLink from './components/MyNavLink';
 export default class App extends Component {
-	render() {
-		return (
-			<div className="container">
-				<Search />
-				<List/>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <Header />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* <NavLink
+                activeClassName="liugezhou"
+                className="list-group-item"
+                to="/about"
+              >
+                About
+              </NavLink>
+              <NavLink
+                activeClassName="liugezhou"
+                className="list-group-item"
+                to="/home"
+              >
+                Home
+              </NavLink> */}
+              <MyNavLink to="/about">About</MyNavLink>
+              <MyNavLink to="/home">Home</MyNavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由 */}
+                <Route path="/about" component={About} />
+                <Route path="/home" component={Home} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
